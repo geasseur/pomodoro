e1ff4b47abe96a75e73f3e14b286746b19d59d4d
geasseur <baptiste.ventura@neuf.fr>
2017-07-21 15:58:12 +0200

    chrono done with slide

--- js/pomodoro.js
+++ js/pomodoro.js
@@ -1,6 +1,79 @@
-var MinuteExo = 20;
-var MinuteBreak = 5;
+var MinuteExo = 0;
+var MinuteBreak = 0;
 var secondes = 60;
+var testExo = false;
+var testBreak = false;
+var timeExo;
+var timeBreak;
+
+function lancementDecompteExo(){
+  $("#remplissageBreak").slideUp();
+  $("#remplissageExo").slideDown(((MinuteExo+1) * secondes)*1000);
+  timeExo = setTimeout(function () {
+    console.log("test2");
+    secondes--;
+    decompteExo();
+  }, 1000);
+}
+
+function decompteExo(){
+    console.log("test3");
+    if (MinuteExo === 0 && secondes === 0) {
+      $("#secondesExo").text(secondes);
+      secondes = 60;
+      lancementDecompteBreak();
+    }
+    else if (secondes === 0) {
+      secondes = 60;
+      $("#secondesExo").text(secondes);
+      MinuteExo--;
+      $("#compteurExo").text(MinuteExo);
+      lancementDecompteExo();
+    }
+    else{
+      console.log(secondes);
+      $("#secondesExo").text(secondes);
+      lancementDecompteExo();
+    }
+}
+
+function lancementDecompteBreak(){
+  $("#remplissageExo").slideUp();
+  $("#remplissageBreak").slideDown(((MinuteBreak+1) * secondes)*1000);
+  timeBreak = setTimeout(function () {
+    console.log("testBreak2");
+    secondes--;
+    decomptebreak();
+  }, 1000);
+}
+
+function decomptebreak(){
+    console.log("test3");
+    if (MinuteExo === 0 && secondes === 0) {
+      $("#secondesBreak").text(secondes);
+      secondes = 60;
+      lancementDecompteExo();
+    }
+    else if (secondes === 0) {
+      secondes = 60;
+      $("#secondesBreak").text(secondes);
+      MinuteExo--;
+      $("#compteurBreak").text(MinuteExo);
+      lancementDecompteBreak();
+    }
+    else{
+      console.log(secondes);
+
+      $("#secondesBreak").text(secondes);
+      lancementDecompteBreak();
+    }
+}
+
+function depart(){
+  $("#compteurBreak").text(MinuteBreak);
+  $("#compteurExo").text(MinuteExo);
+  console.log("test");
+}
 
 $(document).ready(function(){
   depart();
@@ -25,56 +98,19 @@ $(document).ready(function(){
     $("#compteurExo").text(MinuteExo);
   });
   $("#exo").click(function(){
-    lancementDecompteExo();
+    console.log("testExo");
+    if (testExo == true) {
+      testExo = false;
+      clearTimeOut(timeExo);
+    }
+    else {
+      testExo = true;
+      lancementDecompteExo();
+   }
   });
   $("#break").click(function(){
+    console.log("testBreak");
     lancementDecompteBreak();
   });
 
 });
-
-function lancementDecompteExo(){
-  console.log("test");
-  $("#secondesExo").text(secondes);
-  setInterval(decompteExo,1000);
-}
-
-function decompteExo(){
-  while(MinuteExo != 0){
-    secondes--;
-    $("#secondesExo").text(secondes);
-    if (secondes === 0) {
-      secondes = 60;
-      MinuteExo--;
-      $("#compteurExo").text(MinuteExo);
-    }
-  }
-  if (MinuteExo === 0) {
-    lancementDecompteBreak();
-  }
-}
-
-function lancementDecompteBreak(){
-  console.log("test");
-  $("#secondesExo").text(secondes);
-  setInterval(decompteBreak,1000);
-}
-
-function decompteBreak(){
-  while(MinuteBreak != 0){
-    secondes--;
-    $("#secondesExo").text(secondes);
-    if (secondes === 0) {
-      secondes = 60;
-      MinuteBreak--;
-      $("#compteurBreak").text(MinuteBreak);
-    }
-  }
-  lancementDecompteBreak();
-}
-
-function depart(){
-  $("#compteurBreak").text(MinuteBreak);
-  $("#compteurExo").text(MinuteExo);
-  console.log("test");
-}
